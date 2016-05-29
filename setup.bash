fdf563ae1eb868f1a5517dea58bcb8529b37685f
Tomasz Janeczko <janecztom@gmail.com>
2016-05-29 17:59:17 +0000

    Update setup.bash script with comments.

--- setup.bash
+++ setup.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Helper function that outputs to stderr
+# Helper function that outputs to stderr.
 echoerr() { printf "%s\n" "$*" >&2; }
 
 echo "Setting up the Pi."
 
+# Update the packages list, unless the user does not wish to do so.
 if [ "$1" != "noupdate" ]; then
     echo -n "Updating packages list... "
     sudo apt-get update >/dev/null
@@ -13,6 +14,7 @@ else
     echo "Update of packages skipped."
 fi
 
+# Install all the packages from the list, unless already present.
 packages=(vim)
 for package in "${packages[@]}"; do
     if ! dpkg-query -l $package > /dev/null; then
@@ -21,7 +23,7 @@ for package in "${packages[@]}"; do
     fi
 done
 
-# Generate the ssh key first if it doesn't exist
+# Generate the ssh key first if it doesn't exist.
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
     echo "Generating the SSH key in default location"
     ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa
@@ -29,6 +31,7 @@ else
     echoerr "SSH key already exists."
 fi
 
+# Copy a base .vimrc file from base_configs folder.
 if [ ! -f ~/.vimrc ]; then
     echo "Copying a base vimrc file."
     cp base_configs/vimrc ~/.vimrc
